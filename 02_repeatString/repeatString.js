26349b87dd49afad5bc62631f9f034c3d831abac
Davidgnr06 <dgsb2000@gmail.com>
2024-05-31 19:17:54 -0400

    all the 7 done correct with test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
 
-};
+const number = Math.floor(Math.random() * 100);
+const repeatString = function(message, times) {
+    if (times < 0 ){
+        return ( 'ERROR')
+    }
+    let result = '';
+    for (let i = 0; i < times; i++) {
+        result += message;
+               
+    }
+    return( result);
+}
+
+
+repeatString ('', 10)
+
 
 // Do not edit below this line
 module.exports = repeatString;
